65ea4d3c08f2b78268fe2e5e45232a3977a9a5ea
Hide-nu <nkret805worm@gmail.com>
2022-09-18 12:28:37 +0900

    create browser

---
+++ src/dom.rs
@@ -0,0 +1,51 @@
+use std::collections::{HashMap, HashSet};
+
+
+#[derive(Debug)]
+pub struct Node {
+  pub children: Vec<Node>,
+  pub node_type: NodeType,
+}
+
+
+#[derive(Debug)]
+pub enum NodeType {
+  Text(String),
+  Element(ElementData),
+}
+
+
+#[derive(Debug)]
+pub struct ElementData {
+  pub tag_name: String,
+  pub attributes: AttrMap,
+}
+
+impl ElementData {
+  pub fn id(&self) -> Option<&String> {
+    self.attributes.get("id")
+  }
+
+  pub fn classes(&self) -> HashSet<&str> {
+    match self.attributes.get("class") {
+      Some(classlist) => classlist.split(' ').collect(),
+      None => HashSet::new()
+    }
+  }
+}
+
+pub type AttrMap = HashMap<String, String>;
+
+pub fn text(data: String) -> Node {
+  Node {children: Vec::new(), node_type: NodeType::Text(data)}
+}
+
+pub fn elem(name: String, attrs: AttrMap, chiledren: Vec<Node>) -> Node {
+  Node {
+    children: chiledren,
+    node_type: NodeType::Element(ElementData{
+      tag_name: name,
+      attributes: attrs,
+    })
+  }
+}
